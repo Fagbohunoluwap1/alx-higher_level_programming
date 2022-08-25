044c9631be6ced1168e31667439b9a1dfcbccdc2
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-08-25 16:09:16 +0000

    second attempt

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(num - 1))
 
-    for i in range(1, num):
+    for i in range(0, num):
         print("{}: {}".format(i, sys.argv[i]))
