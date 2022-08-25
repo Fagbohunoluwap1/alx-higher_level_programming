fb69abe5eeb2eeeecaff0996278d932680c6b24f
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-08-25 19:59:12 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    """prints all names defined  by hiddden_4"""
+    """prints all names defined  by hidden_4 module"""
     import hidden_4
 
     names = dir(hidden_4)
