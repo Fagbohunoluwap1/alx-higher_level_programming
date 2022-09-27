aae8d1baf44afa3015f40c290bd36981a2938aa9
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-09-27 16:23:19 -0700

    student to json

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -15,6 +15,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """Get a dictionary representation of the student."""
         return self.__dict__
