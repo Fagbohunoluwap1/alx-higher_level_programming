a8354432db0f496ec5d0b079568752876785f092
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-09-27 14:28:59 -0700

    reads a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""Defines a text file reading function"""
 
+
+def read_file(filename=""):
+    """Print the content of a UTF-8 text file to stdout."""
+
+    with open(filename, encoding="UTF-8") as f:
+        print(f.read(), end="")
