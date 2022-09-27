ccca029cafe84298cfdd44770918c9651ad8a42d
fagbohunoluwap1 <holuwharphelumhe01@gmail.com>
2022-09-27 14:58:01 -0700

    serialization

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines the string to JSON function"""
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of a string object"""
+    return json.dumps(my_obj)
